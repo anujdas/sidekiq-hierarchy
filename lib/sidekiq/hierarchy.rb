34ca98306b81a9073a1f41b45e01ba3fb95771da
Anuj Das <anujdas@gmail.com>
2015-09-24 15:42:18 -0700

    Initial commit

---
+++ lib/sidekiq/hierarchy.rb
@@ -0,0 +1,6 @@
+require "sidekiq/hierarchy/version"
+
+module Sidekiq
+  module Hierarchy
+  end
+end
---
+++ lib/sidekiq/hierarchy/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module Hierarchy
+    VERSION = "0.0.#{ENV['BUILD_NUMBER'] || 'dev'}"
+  end
+end
