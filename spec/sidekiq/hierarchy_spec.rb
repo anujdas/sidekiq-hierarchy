12a187f61394e910de597f69803e3d1177bcfe5f
Anuj Das <anujdas@gmail.com>
2015-09-24 15:47:55 -0700

    Fix tests ;)

--- spec/sidekiq/hierarchy_spec.rb
+++ spec/sidekiq/hierarchy_spec.rb
@@ -1,11 +1,4 @@
 require 'spec_helper'
 
 describe Sidekiq::Hierarchy do
-  it 'has a version number' do
-    expect(Sidekiq::Hierarchy::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
