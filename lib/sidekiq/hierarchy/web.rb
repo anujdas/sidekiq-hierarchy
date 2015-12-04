163987c185370c442be45d62b3ee6689125c3952
Anuj Das <anujdas@gmail.com>
2015-12-04 14:49:13 -0800

    Extract web helpers to a separate file

--- lib/sidekiq/hierarchy/web.rb
+++ lib/sidekiq/hierarchy/web.rb
@@ -3,62 +3,12 @@
 # not to leave this open in a tab forever.
 # Sidekiq seems to use Bootstrap 3.0.0 currently; find docs at
 # http://bootstrapdocs.com/v3.0.0/docs/
+
+require 'sidekiq/hierarchy/web/helpers'
+
 module Sidekiq
   module Hierarchy
     module Web
-      module Helpers
-        # Override find_template logic to process arrays of view directories
-        # warning: this may be incompatible with other overrides of find_template,
-        # though that really shouldn't happen if they match the method contract
-        def find_template(views, name, engine, &block)
-          Array(views).each do |view_dir|
-            super(view_dir, name, engine, &block)
-          end
-        end
-
-        def job_url(job=nil)
-          "#{root_path}hierarchy/jobs/#{job.jid if job}"
-        end
-
-        def workflow_url(workflow=nil)
-          "#{root_path}hierarchy/workflows/#{workflow.jid if workflow}"
-        end
-
-        def workflow_set_url(status)
-          "#{root_path}hierarchy/workflow_sets/#{status}"
-        end
-
-        def safe_relative_time(timestamp)
-          timestamp.nil? ? '-' : relative_time(timestamp)
-        end
-
-        def status_updated_at(job)
-          case job.status
-          when :enqueued
-            job.enqueued_at
-          when :running, :requeued
-            job.run_at
-          when :complete
-            job.complete_at
-          when :failed
-            job.failed_at
-          end
-        end
-
-        def bootstrap_status(status)
-          case status
-          when :enqueued, :requeued
-            'warning'
-          when :running
-            'info'
-          when :complete
-            'success'
-          when :failed
-            'danger'
-          end
-        end
-      end
-
       VIEW_PATH = File.expand_path('../../../../web/views', __FILE__)
       PER_PAGE = 20
 
